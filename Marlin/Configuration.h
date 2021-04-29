30068b44110da5f938f74eb3863256879372ade1
jfodor1975 <jeff.fodor@gmail.com>
2021-04-29 14:40:47 -0400

    Update Configuration.h
    updated hotend PID

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -490,9 +490,12 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   //Flsun hotend
-  #define DEFAULT_Kp 13.27
-  #define DEFAULT_Ki 0.55
-  #define DEFAULT_Kd 79.91
+  
+  #define DEFAULT_Kp 41.83
+  #define DEFAULT_Ki 5.67
+  #define DEFAULT_Kd 77.10
+
+
 
   // Ultimaker
   //#define DEFAULT_Kp 22.2
