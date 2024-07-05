040f48f5d7e3d4fc1cec52a55743b42097cdde94
jfodor1975 <jeff.fodor@gmail.com>
2024-07-05 15:18:29 -0400

    filament estep calibrated

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1168,7 +1168,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 99.61, 400, 697.97 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 99.61, 400, 683.95 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
